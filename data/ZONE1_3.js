7f3191ab6c894b71cb1003804149b9b805479b14
wepmaping <76062307+wepmaping@users.noreply.github.com>
2020-12-15 17:33:49 +0000

    Add files via upload

---
+++ data/ZONE1_3.js
@@ -0,0 +1,8 @@
+var json_ZONE1_3 = {
+"type": "FeatureCollection",
+"name": "ZONE1_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "ENSEMBLE1", "superficie": "200710", "type": "ilot230", "propiètair": "mr yao" }, "geometry": { "type": "Polygon", "coordinates": [ [ [ -3.889485105340945, 5.371845434605806, 0.0 ], [ -3.885215746454014, 5.373696363852181, 0.0 ], [ -3.884130540432799, 5.372801813062437, 0.0 ], [ -3.88448251737691, 5.369618392638048, 0.0 ], [ -3.88970469233864, 5.36997293153837, 0.0 ], [ -3.889485105340945, 5.371845434605806, 0.0 ] ] ] } }
+]
+}
